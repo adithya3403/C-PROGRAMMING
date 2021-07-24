c8bd78eb73d098a6500a2e7eb961623d6fa96802
Adithya K <a.dty.3403@gmail.com>
2021-07-24 19:10:52 +0530

    Add files via upload

---
+++ LAB/FILES/printContentsFile.c
@@ -0,0 +1,26 @@
+// C Program to print contents of file
+#include <stdio.h>
+#include <stdlib.h>
+void main()
+{
+    FILE *fptr;
+    char filename[100], c;
+    printf("Enter the filename to open \n");
+    //  With extension
+    scanf("%s", filename);
+    // Open file
+    fptr = fopen(filename, "r");
+    if (fptr == NULL)
+    {
+        printf("Cannot open file \n");
+        exit(0);
+    }
+    // Read contents from file
+    c = fgetc(fptr);
+    while (c != EOF)
+    {
+        printf ("%c", c);
+        c = fgetc(fptr);
+    }
+    fclose(fptr);
+}
