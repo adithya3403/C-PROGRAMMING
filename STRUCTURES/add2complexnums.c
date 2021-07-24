65af53d6a17c2a0a05ae0fb7125f71d4de6f4439
Adithya K <a.dty.3403@gmail.com>
2021-07-24 10:35:01 +0530

    structures codes

---
+++ STRUCTURES/add2complexnums.c
@@ -0,0 +1,20 @@
+// Write a c program to read , add two complex numbers and display the result. Where as structure declaration in global scope use typedef declaration style.
+
+#include<stdio.h> 
+typedef struct
+{
+    int real; 
+    int imag;
+} COMPLEX;
+COMPLEX C1, C2, Result_C;
+void main( )
+{
+    printf("\n Enter the real and imaginary parts of the first complex number:   ");
+    scanf("%d%d", &C1.real , &C1.imag);
+    printf("\n Enter the real and imaginary parts of the second complex number:   "); 
+    scanf("%d%d", &C2.real , &C2.imag);
+    Result_C.real = C1.real + C2.real; 
+    Result_C.imag = C1.imag + C2.imag; 
+    printf("\n");
+    printf( " \n The sum of two complex numbers is: %d + %di",Result_C.real,Result_C.imag);
+}
