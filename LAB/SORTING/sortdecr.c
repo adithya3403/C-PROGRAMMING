d49a08a324b6c4dab61d6b39e404500bc7278572
Adithya K <a.dty.3403@gmail.com>
2021-07-24 19:08:03 +0530

    Add files via upload

---
+++ LAB/SORTING/sortdecr.c
@@ -0,0 +1,26 @@
+// Write a C program that implements the selection sort method to sort a given list of integers in descending order.
+#include<stdio.h>
+void main()
+{
+    int n, a[20], min, temp, i, j;
+    printf("Enter the size of the array\n");
+    scanf("%d", &n);
+    printf("Enter the array elements\n");
+    for(i = 0; i < n; i++)
+        scanf("%d", &a[i]);
+    for(i = 0; i < n - 1; i++)
+    {
+        min = i;
+        for(j = i + 1; j < n; j++)
+        {
+            if(a[j] > a[min])
+            min = j;
+        }
+        temp = a[i];
+        a[i] = a[min];
+        a[min] = temp;
+    }
+    printf("The sorted array is\n");
+    for(i = 0; i < n; i++)
+        printf("%d ", a[i]);
+}
