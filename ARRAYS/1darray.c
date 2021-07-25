25c7dca3120a3e707febfe858a9f3fd972d6e74e
Adithya <a.dty.3403@gmail.com>
2021-07-25 12:31:28 +0530

    init

--- ARRAYS/1darray.c
+++ ARRAYS/1darray.c
@@ -13,3 +13,12 @@ void main()
         printf("Element stored at a[%d]=%d \n", i, array[i]); 
     } 
 }
+
+// Enter 5 numbers to store them in array 
+// 1 2 3 4 5
+// Element in the array are:
+// Element stored at a[0]=1 
+// Element stored at a[1]=2 
+// Element stored at a[2]=3 
+// Element stored at a[3]=4 
+// Element stored at a[4]=5 
