e350a1695548b024fca248a0ec25ce77339185d5
Adithya K <a.dty.3403@gmail.com>
2021-08-07 10:53:02 +0530

    ip op

--- LAB/ARRAYS_POINTERS_FUNCTIONS/MATRICES/add2matrices.c
+++ LAB/ARRAYS_POINTERS_FUNCTIONS/MATRICES/add2matrices.c
@@ -1,40 +1,42 @@
 // Write a C program that uses functions to perform the following: Addition of Two Matrices
 #include <stdio.h>
-int main()
-{
-    int rowCount, columnCount, i, j;
-    int firstMatrix[10][10], secondMatrix[10][10], resultMatrix[10][10];
-    printf("Number of rows of matrices to be added : ");
-    scanf("%d", &rowCount);
-    printf("Number of columns matrices to be added : ");
-    scanf("%d", &columnCount);
-    printf("Elements of first matrix : \n");
-    for (i = 0; i < rowCount; i++)
-        for (j = 0; j < columnCount; j++)
-            scanf("%d", &firstMatrix[i][j]);
-    printf("Elements of second matrix : \n");
-    for (i = 0; i < rowCount; i++)
-        for (j = 0; j < columnCount; j++)
-            scanf("%d", &secondMatrix[i][j]);
-    printf("Sum of entered matrices : \n");
-    for (i = 0; i < rowCount; i++)
-    {
-        for (j = 0; j < columnCount; j++)
-        {
-            resultMatrix[i][j] = firstMatrix[i][j] + secondMatrix[i][j];
-            printf("%d\t",resultMatrix[i][j]);
+void main() {
+    int r, c;
+    int A[10][10], B[10][10], C[10][10];
+    printf("Enter number of rows : ");
+    scanf("%d", &r);
+    printf("Enter number of columns : ");
+    scanf("%d", &c);
+    printf("Enter elements of first matrix : ");
+    for (int i = 0; i < r; i++) {
+        for (j = 0; j < c; j++) {
+            scanf("%d", &A[i][j]);
+        }
+    }
+    printf("Enter elements of second matrix : ");
+    for (int i = 0; i < r; i++) {
+        for (int j = 0; j < c; j++) {
+            scanf("%d", &B[i][j]);
+        }
+    }
+    for (int i = 0; i < r; i++) {
+        for (int j = 0; j < c; j++) {
+            C[i][j] = A[i][j] + B[i][j];
+        }
+    }
+    printf("On adding both the matrices, we get : \n");
+    for (int i = 0; i < r; i++) {
+        for (int j = 0; j < c; j++) {
+            printf("%d ",C[i][j]);
         }
         printf("\n");
     }
-    return 0;
 }
 
-// Number of rows of matrices to be added : 2
-// Number of columns matrices to be added : 2
-// Elements of first matrix : 
-// 1 2 3 4
-// Elements of second matrix : 
-// 5 6 7 8
+// Enter number of rows : 2
+// Enter number of columns : 2
+// Enter elements of first matrix : 1 2 3 4
+// Enter elements of second matrix : 5 6 7 8
 // Sum of entered matrices :
-// 6       8
-// 10      12
+// 6 8
+// 10 12
