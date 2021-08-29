8d365abd10c6e7ebb35fe4ffe084b022e078937a
Adithya K <a.dty.3403@gmail.com>
2021-08-29 23:09:05 +0530

    Update h.c

--- LAB/08_sorting and searching/h.c
+++ LAB/08_sorting and searching/h.c
@@ -3,9 +3,9 @@
 void main()
 {
     int n, array[1000], c, d, t;
-    printf("Enter number of elements\n");
+    printf("Enter number of elements : ");
     scanf("%d", &n);
-    printf("Enter %d integers\n", n);
+    printf("Enter %d integers : ", n);
     for (c = 0; c < n; c++)
         scanf("%d", &array[c]);
     for (c = 1 ; c <= n - 1; c++)
@@ -19,14 +19,11 @@ void main()
             d--;
         }
     }
-    printf("Sorted list in ascending order:\n");
+    printf("Sorted list in ascending order : ");
     for (c = 0; c <= n - 1; c++)
         printf("%d ", array[c]);
 }
 
-// Enter number of elements
-// 6
-// Enter 6 integers
-// 1 2 5 3 4 6
-// Sorted list in ascending order:
-// 1 2 3 4 5 6
+// Enter number of elements : 6
+// Enter 6 integers : 1 2 5 3 4 6
+// Sorted list in ascending order : 1 2 3 4 5 6
