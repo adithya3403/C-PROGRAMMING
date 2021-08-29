380cbea72fb302c4330149081f62d887db0a61aa
Adithya K <a.dty.3403@gmail.com>
2021-08-29 23:07:43 +0530

    Update g.c

--- LAB/08_sorting and searching/g.c
+++ LAB/08_sorting and searching/g.c
@@ -3,9 +3,9 @@
 void main()
 {
     int n, a[20], min, temp, i, j;
-    printf("Enter the size of the array\n");
+    printf("Enter the size of the array : ");
     scanf("%d", &n);
-    printf("Enter the array elements\n");
+    printf("Enter the array elements : ");
     for(i = 0; i < n; i++)
         scanf("%d", &a[i]);
     for(i = 0; i < n - 1; i++)
@@ -20,14 +20,11 @@ void main()
         a[i] = a[min];
         a[min] = temp;
     }
-    printf("The sorted array is\n");
+    printf("The sorted array is : ");
     for(i = 0; i < n; i++)
         printf("%d ", a[i]);
 }
 
-// Enter the size of the array
-// 6
-// Enter the array elements
-// 1 2 3 4 8 5
-// The sorted array is
-// 8 5 4 3 2 1
+// Enter the size of the array : 6
+// Enter the array elements : 1 2 3 4 5 8
+// The sorted array is : 8 5 4 3 2 1
